6643c997a7ce7736dcd703cf5fbc1b828aee4ac1
nk00374 <nk00374@surrey.ac.uk>
2019-04-15 16:39:14 +0100

    Attempt in adding a dialog for adding photos

--- app/src/main/java/com/example/softeng/recipick/Fragments/RecipePhotosFragment.java
+++ app/src/main/java/com/example/softeng/recipick/Fragments/RecipePhotosFragment.java
@@ -1,15 +1,31 @@
 package com.example.softeng.recipick.Fragments;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
+import com.example.softeng.recipick.Adapters.ImageListAdapter;
 import com.example.softeng.recipick.R;
+import java.util.List;
+
+import es.dmoral.toasty.Toasty;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -20,6 +36,8 @@ import com.example.softeng.recipick.R;
  * create an instance of this fragment.
  */
 public class RecipePhotosFragment extends Fragment {
+
+    private static final String TAG = "Recipe Photos";
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -30,7 +48,38 @@ public class RecipePhotosFragment extends Fragment {
     private String mParam2;
 
 
-    private Button addBtn;
+
+    /** The request code in order for the
+     *  user to select multiple images,
+     *  capture an image using the camera,
+     *  to have access to external storage*/
+    private static final int RESULT_LOAD_IMAGE = 1;
+    private static final int REQUEST_CAPTURE_IMAGE = 100;
+    private final int MY_PERMISSIONS_REQUEST_CAMERA = 1;
+    private final int MY_PERMISSIONS_REQUEST_EXTERNAL_STORAGE = 2;
+
+
+    /** Holds the list of file names     */
+    private List<String> fileNameList;
+    /** Holds the list of file directories as URI */
+    private List<Uri> fileList;
+
+
+    private Uri imageUri;
+
+    private ImageListAdapter adapter;
+
+    private Button btnOpenDialog;
+
+
+    /** Recycler view will hold the list of selected images */
+    private RecyclerView mImages;
+
+
+    /** Allows us to create custom dialogs */
+    private AlertDialog.Builder mBuilder;
+    /** Allows us to create custom dialogs */
+    private View mView;
 
 
     private OnFragmentInteractionListener mListener;
@@ -69,23 +118,43 @@ public class RecipePhotosFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        addBtn = container.findViewById(R.id.btnAdd);
+        View view =  inflater.inflate(R.layout.fragment_recipe_photos, container, false);
 
+        if (view != null) {
+            btnOpenDialog = view.findViewById(R.id.btnOpenDialog);
+            Log.d(TAG, "view is not null");
 
-        addBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                createDialog();
+            if (btnOpenDialog != null) {
+                btnOpenDialog.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toasty.info(requireContext(), "no work", Toast.LENGTH_SHORT, true).show();
+
+                        createDialog();
+                    }
+                });
+                Log.d(TAG, "mButton is not null");
             }
-        });
+        }
 
-        return inflater.inflate(R.layout.fragment_recipe_photos, container, false);
+
+        return view;
     }
+
 
     public void createDialog() {
+        Toasty.info(requireContext(), "no work", Toast.LENGTH_SHORT, true).show();
+
+        mBuilder = new AlertDialog.Builder(requireContext());
+        mView = getLayoutInflater().inflate(R.layout.dialog_add_photos, null);
+
+        /** The dialog is displayed and shown to the user*/
+        mBuilder.setView(mView);
+        final AlertDialog dialog = mBuilder.create();
+        dialog.show();
 
     }
+
 
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
