a895cd20dbc2c278e4bb2d8d296f175fc603988d
Lekkas, Pavlos (UG - Computer Science) <pl00349@surrey.ac.uk>
2019-04-17 15:54:03 +0300

    Unit tests

--- app/src/test/java/com/example/softeng/recipick/Models/ReviewTest.java
+++ app/src/test/java/com/example/softeng/recipick/Models/ReviewTest.java
@@ -1,6 +1,8 @@
 package com.example.softeng.recipick.Models;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
@@ -13,10 +15,27 @@ class ReviewTest {
     @Mock
     List<String> images;
     @InjectMocks
-    Review review;
+    Review review = new Review(images, "author", 10);
 
     @BeforeEach
     void setUp() {
         MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    void TestGetAuthor() {
+        review.setAuthor("TestName");
+        String result = review.getAuthor();
+        Assertions.assertEquals("TestName", result);
+
+    }
+
+    @Test
+    void TestGetRating() {
+        int expected = 10;
+        review.setRating(expected);
+        int result = review.getRating();
+        Assertions.assertEquals(expected, result);
+
     }
 }
