7a7c5fe406ee3904793a4963dabaecf56459fd8e
Nithesh Koneswaran <nk00374@surrey.ac.uk>
2019-04-20 21:41:18 +0100

    Nothing has changed

--- app/src/main/java/com/example/softeng/recipick/Models/Utility.java
+++ app/src/main/java/com/example/softeng/recipick/Models/Utility.java
@@ -1,27 +1,20 @@
 package com.example.softeng.recipick.Models;
-
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
-import android.util.Log;
-import android.widget.EditText;
-import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.CountDownLatch;
 
-import es.dmoral.toasty.Toasty;
 
 public class Utility {
 
+    public static final String USERFAVOURITES = "User_Favourites";
     public static final String USERS = "Users";
     public static final String DISPLAYNAME = "display_name";
     public static final String EMAIL = "email";
@@ -29,12 +22,14 @@ public class Utility {
     public static final String FAVOURITES = "favourites";
     public static final String TROLLEY = "trolley";
     public static final String RECIPE = "RECIPE";
+    public static final String ID = "ID";
 
-    private static User user;
+    public static User user;
 
 
 
     private DocumentReference userRef;
+
 
     public static boolean loggedIn() {
         FirebaseAuth mAuth = FirebaseAuth.getInstance();
@@ -57,9 +52,8 @@ public class Utility {
         if (!Utility.loggedIn()) {
             return;
         }
-
         DocumentReference userRef = FirebaseFirestore.getInstance().collection(USERS).document(getUid());
-       // CountDownLatch done = new CountDownLatch(1);
+       /** Could turn this into a listener */
         userRef.get()
                 .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
                     @Override
@@ -79,8 +73,8 @@ public class Utility {
                                 }
                                 StringBuilder favourites = new StringBuilder();
                                 if (user.getFavourites()!=null) {
-                                    for (String recipe : user.getFavourites()) {
-                                        favourites.append(recipe).append(",");
+                                    for (String id : user.getFavourites().keySet()) {
+                                        favourites.append(id).append(",");
                                     }
                                 }
                                 StringBuilder trolleyList = new StringBuilder();
@@ -146,6 +140,8 @@ public class Utility {
         }
         return exists;
     }
+
+
     public static void updateUserTrolley(final Context context, List<String> trolley) {
         SharedPreferences prefs = context.getSharedPreferences("com.example.softeng.recipick", Context.MODE_PRIVATE);
         StringBuilder list = new StringBuilder();
