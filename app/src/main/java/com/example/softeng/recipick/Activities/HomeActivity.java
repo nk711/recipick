b1b5dc80ac0cbd0c32979592a63e30ad40bd90ea
Leal, Clyde D (UG - Computer Science) <cl01056@surrey.ac.uk>
2019-05-11 15:43:54 +0100

    Removed the options menu in the main page, since it is no longer needed.

--- app/src/main/java/com/example/softeng/recipick/Activities/HomeActivity.java
+++ app/src/main/java/com/example/softeng/recipick/Activities/HomeActivity.java
@@ -159,30 +159,6 @@ public class HomeActivity extends AppCompatActivity {
 
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_home, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        switch (item.getItemId()) {
-            case R.id.signOut:
-                mAuth.signOut();
-                finish();
-                startActivity(new Intent(this, LoginActivity.class));
-                break;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     /**
      * A placeholder fragment containing a simple view.
      */
