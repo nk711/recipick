117ad547bdc34f9b4d3a29a0686b891e9dfd0a3d
Leal, Clyde D (UG - Computer Science) <cl01056@surrey.ac.uk>
2019-05-13 00:33:12 +0100

    Added comments for Ingredient, Recipe and User model classes.

--- app/src/main/java/com/example/softeng/recipick/Models/User.java
+++ app/src/main/java/com/example/softeng/recipick/Models/User.java
@@ -1,3 +1,6 @@
+/**
+ * User.java
+ */
 package com.example.softeng.recipick.Models;
 
 
@@ -8,18 +11,26 @@ import java.util.Map;
  * An account using the app
  */
 public class User {
+    /** Holds the display name of user */
     private String display_name;
+    /** Holds the user's email address */
     private String email;
+    /** Holds the user's ingredients */
     private Map<String, Boolean> ingredients;
+    /** Holds the user's favourite recipes */
     private Map<String, Recipe> favourites;
+    /** Holds the user's trolley */
     private Map<String, Boolean> trolley;
 
+    /**
+     * A default constructor
+     */
     public User() {
 
     }
 
     /**
-     * Constructor method
+     * A parameterised constructor that initialises the fields.
      * @param display_name Name shown to other users
      * @param email Email address linked to account
      */
@@ -31,43 +42,82 @@ public class User {
         this.trolley = new HashMap<>();
     }
 
-    //Getters and Setters
+    /**
+     * An accessor for the user's favourites
+     * @return - user's favourite recipes
+     */
     public Map<String, Recipe> getFavourites() {
         return favourites;
     }
 
+    /**
+     * A mutator for the user's favourites
+     * @param favourites
+     */
     public void setFavourites(Map<String, Recipe> favourites) {
         this.favourites = favourites;
     }
 
+    /**
+     * An accessor for the user's ingredients
+     * @return - user's ingredients
+     */
     public  Map<String, Boolean> getIngredients() {
         return this.ingredients;
     }
 
+    /**
+     * A mutator for the user's ingredients
+     * @param ingredients
+     */
     public void setIngredients( Map<String, Boolean> ingredients) {
         this.ingredients = ingredients;
     }
 
+    /**
+     * An accessor for the user's trolley
+     * @return - ingredients in user's trolley
+     */
     public Map<String, Boolean> getTrolley() {
         return this.trolley;
     }
 
+    /**
+     * A mutator for the user's trolley
+     * @param trolley
+     */
     public void setTrolley(Map<String, Boolean> trolley) {
         this.trolley = trolley;
     }
 
+    /**
+     * An accessor for the user's display name
+     * @return - display name
+     */
     public String getDisplay_name() {
         return display_name;
     }
 
+    /**
+     * A mutator for the user's display name
+     * @param display_name
+     */
     public void setDisplay_name(String display_name) {
         this.display_name = display_name;
     }
 
+    /**
+     * An accessor for the user's email address
+     * @return - user's email address
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * A mutator for the user's email
+     * @param email
+     */
     public void setEmail(String email) {
         this.email = email;
     }
